ff4273e73abdd551b63a5b171a4b57cd2ba30ec8
Mohankumar <91587795+et-mohan@users.noreply.github.com>
2024-02-14 21:26:01 +0530

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,7 @@
 'use strict';
 
+
+
 // element toggle function
 const elementToggleFunc = function (elem) { elem.classList.toggle("active"); }
 
@@ -13,7 +15,7 @@ sidebarBtn.addEventListener("click", function () { elementToggleFunc(sidebar); }
 // page navigation variables
 const navigationLinks = document.querySelectorAll("[data-nav-link]");
 const pages = document.querySelectorAll("[data-page]");
-console.log(pages)
+// console.log(pages)
 // add event to all nav link
 for (let i = 0; i < navigationLinks.length; i++) {
   navigationLinks[i].addEventListener("click", function () {
